0cc49eced31692572020e55d96d1d71338404ae7
Davi Lima <davi.n.lima99@gmail.com>
2021-03-23 09:40:19 -0300

    Add list of employees

---
+++ src/main/java/com/davilm/springboot/backend/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.davilm.springboot.backend.controller;
+
+import com.davilm.springboot.backend.model.Employee;
+import com.davilm.springboot.backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+
+
+}
